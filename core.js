438a7d4c9a4771039d40d4e602f5f9b637be5a69
hcz <h.cz@qq.com>
2016-09-05 16:23:45 -0400

    modify and add visitor.js

--- core.js
+++ core.js
@@ -17,86 +17,83 @@
 // lookup mode:
 //     global, mixed, local
 
-module.exports.literal = (type, value) => {
-    return {
-        astType: () => {
-            return 'literal';
-        },
-        dataType: () => {
-            return type;
-        },
-        value: () => {
-            return value;
-        },
-    };
-};
-
-module.exports.instance = (code, types) => {
-    return {
-        astType: () => {
-            return 'instance';
-        },
-        code: () => {
-            return code;
-        },
-        types: () => {
-            return types;
-        },
-    };
-};
-
-module.exports.root = (mode) => {
-    return {
-        astType: () => {
-            return 'root';
-        },
-        mode: () => {
-            return mode;
-        },
-    };
-};
-
-module.exports.path = (source, name) => {
-    return {
-        astType: () => {
-            return 'path';
-        },
-        source: () => {
-            return source;
-        },
-        name: () => {
-            return name;
-        },
-    };
-};
-
-module.exports.call = (source, args) => {
-    return {
-        astType: () => {
-            return 'call';
-        },
-        source: () => {
-            return source;
-        },
-        args: () => {
-            return args;
-        },
-    };
-};
-
-module.exports.code = (source, params, ast) => {
-    return {
-        astType: () => {
-            return 'code';
-        },
-        source: () => {
-            return source;
-        },
-        params: () => {
-            return params;
-        },
-        ast: () => {
-            return ast;
-        },
-    };
+module.exports = {
+    literal: (type, value) => {
+        return {
+            astType: () => {
+                return 'literal';
+            },
+            dataType: () => {
+                return type;
+            },
+            value: () => {
+                return value;
+            },
+        };
+    },
+    instance: (code, types) => {
+        return {
+            astType: () => {
+                return 'instance';
+            },
+            code: () => {
+                return code;
+            },
+            types: () => {
+                return types;
+            },
+        };
+    },
+    root: (mode) => {
+        return {
+            astType: () => {
+                return 'root';
+            },
+            mode: () => {
+                return mode;
+            },
+        };
+    },
+    path: (source, name) => {
+        return {
+            astType: () => {
+                return 'path';
+            },
+            source: () => {
+                return source;
+            },
+            name: () => {
+                return name;
+            },
+        };
+    },
+    call: (source, args) => {
+        return {
+            astType: () => {
+                return 'call';
+            },
+            source: () => {
+                return source;
+            },
+            args: () => {
+                return args;
+            },
+        };
+    },
+    code: (source, params, ast) => {
+        return {
+            astType: () => {
+                return 'code';
+            },
+            source: () => {
+                return source;
+            },
+            params: () => {
+                return params;
+            },
+            ast: () => {
+                return ast;
+            },
+        };
+    },
 };
