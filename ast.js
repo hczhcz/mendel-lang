0fab91a4ca40e5c95d6d0dff91d853aedfaec20d
hcz <h.cz@qq.com>
2016-09-06 19:19:23 -0400

    add spec

--- ast.js
+++ ast.js
@@ -1,22 +1,5 @@
 'use strict';
 
-// data types:
-//     void
-//     bool
-//     i8 i16 i32 i64
-//     u8 u16 u32 u64
-//     f32 f64
-//     str
-//     instance<...>
-// ast node types:
-//     literal
-//     symbol
-//     path
-//     call
-//     code
-// lookup modes:
-//     global, mixed, local
-
 module.exports = {
     literal: (type, value) => {
         return {
