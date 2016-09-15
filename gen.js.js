57c5bbc2a6c595844716c04ec70404cc3799a939
hcz <h.cz@qq.com>
2016-09-15 01:06:29 -0400

    working

--- gen.js.js
+++ gen.js.js
@@ -2,7 +2,32 @@
 
 module.exports = {
     literal: (root, instance, ast) => {
-        //
+        switch (ast.type) {
+            case 'void': {
+                return ['undefined'];
+            }
+            case 'boolean':
+            case 'i8':
+            case 'i16':
+            case 'i32':
+            case 'u8':
+            case 'u16':
+            case 'u32':
+            case 'f32':
+            case 'f64': {
+                return [ast.value.toString()];
+            }
+            case 'string': {
+                return [JSON.stringify(ast.value)];
+            }
+            case 'i64':
+            case 'u64': {
+                throw 1;
+            }
+            default: {
+                throw 1; // never reach
+            }
+        }
     },
 
     self: (root, instance, ast) => {
