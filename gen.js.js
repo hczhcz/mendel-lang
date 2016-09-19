029c335e41570fdb08ea55cfe52f449c1b80ff33
hcz <h.cz@qq.com>
2016-09-19 17:07:12 -0400

    working

--- gen.js.js
+++ gen.js.js
@@ -4,7 +4,7 @@ module.exports = {
     literal: (root, instance, ast) => {
         switch (ast.type) {
             case 'void': {
-                return ['undefined'];
+                return 'undefined';
             }
             case 'boolean':
             case 'i8':
@@ -15,10 +15,10 @@ module.exports = {
             case 'u32':
             case 'f32':
             case 'f64': {
-                return [ast.value.toString()];
+                return ast.value.toString();
             }
             case 'string': {
-                return [JSON.stringify(ast.value)];
+                return JSON.stringify(ast.value);
             }
             case 'i64':
             case 'u64': {
@@ -31,19 +31,23 @@ module.exports = {
     },
 
     self: (root, instance, ast) => {
-        //
+        return '__self';
     },
 
     root: (root, instance, ast) => {
-        //
+        return '__root';
     },
 
     pathOut: (root, instance, ast) => {
-        //
+        const upperJs = module.exports.visit(ast.upper);
+
+        return upperJs + '.get\(' + JSON.stringify(ast.name) + ')';
     },
 
     pathIn: (root, instance, ast) => {
-        //
+        const upperJs = module.exports.visit(ast.upper);
+
+        return upperJs + '.get\(' + JSON.stringify(ast.name) + ')';
     },
 
     callOut: (root, instance, ast) => {
