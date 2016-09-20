84a0349965360d6f454aac396ad4efa399185173
hcz <h.cz@qq.com>
2016-09-20 11:47:58 -0400

    working

--- gen.js.js
+++ gen.js.js
@@ -1,68 +1,107 @@
 'use strict';
 
-module.exports = {
-    literal: (root, instance, ast, target) => {
-        switch (ast.type) {
-            case 'void': {
-                return [target + ' = undefined'];
+module.exports = (out) => {
+    return {
+        literal: (root, instance, ast, target) => {
+            switch (ast.type) {
+                case 'void': {
+                    out.line(target('undefined'));
+                }
+                case 'boolean':
+                case 'i8':
+                case 'i16':
+                case 'i32':
+                case 'u8':
+                case 'u16':
+                case 'u32':
+                case 'f32':
+                case 'f64': {
+                    out.line(target(ast.value.toString()));
+                }
+                case 'string': {
+                    out.line(target(JSON.stringify(ast.value)));
+                }
+                case 'i64':
+                case 'u64': {
+                    throw 1;
+                }
+                default: {
+                    throw 1; // never reach
+                }
             }
-            case 'boolean':
-            case 'i8':
-            case 'i16':
-            case 'i32':
-            case 'u8':
-            case 'u16':
-            case 'u32':
-            case 'f32':
-            case 'f64': {
-                return [target + ' = ' + ast.value.toString()];
+        },
+
+        self: (root, instance, ast, target) => {
+            out.line(target('self'));
+        },
+
+        root: (root, instance, ast, target) => {
+            out.line(target('root'));
+        },
+
+        pathOut: (root, instance, ast, target) => {
+            module.exports.visit(
+                root, instance, ast.upper,
+                (value) => {
+                    return 'upper = ' + value;
+                }
+            );
+
+            out.line(target('upper.get(' + JSON.stringify(ast.name) + ')'));
+        },
+
+        pathIn: (root, instance, ast, value) => {
+            module.exports.visit(
+                root, instance, ast.upper,
+                (value) => {
+                    return 'upper = ' + value;
+                }
+            );
+
+            out.line('upper.set(' + JSON.stringify(ast.name) + ', ' + value + ')');
+        },
+
+        call: (root, instance, ast, target) => {
+            module.exports.visit(
+                root, instance, ast.callee,
+                'callee'
+            );
+
+            out.line('inner = new Map()');
+
+            for (const i in ast.outArgs) {
+                module.exports.visit(
+                    root, instance, ast.callee,
+                    (value) => {
+                        return 'inner.set(' + JSON.stringify(i) + ', ' + value + ')';
+                    }
+                );
             }
-            case 'string': {
-                return [target + ' = ' + JSON.stringify(ast.value)];
+
+            // TODO
+
+            for (const i in ast.inArgs) {
+                module.exports.visit(
+                    root, instance, ast.callee,
+                    'inner.get(' + JSON.stringify(i) + ')'
+                );
             }
-            case 'i64':
-            case 'u64': {
-                throw 1;
-            }
-            default: {
-                throw 1; // never reach
-            }
-        }
-    },
+        },
 
-    self: (root, instance, ast, target) => {
-        return [target + ' = __self'];
-    },
+        callOut: (root, instance, ast, target) => {
+            //
+        },
 
-    root: (root, instance, ast, target) => {
-        return [target + ' = __root'];
-    },
+        callIn: (root, instance, ast, value) => {
+            //
+        },
 
-    pathOut: (root, instance, ast, target) => {
-        const upper = module.exports.visit(root, instance, ast.upper, '__upper');
+        visit: (root, instance, ast, target) => {
+            module.exports[ast.__type](root, instance, ast, target);
+        },
 
-        upper.push(target + ' = __upper.get(' + JSON.stringify(ast.name) + ')');
-    },
-
-    pathIn: (root, instance, ast, target) => {
-        const upper = module.exports.visit(root, instance, ast.upper, '__upper');
-
-        upper.push('__upper.set(' + JSON.stringify(ast.name) + ', ' + target + ')');
-    },
-
-    callOut: (root, instance, ast, target) => {
-        //
-    },
-
-    callIn: (root, instance, ast, target) => {
-        //
-    },
-
-    visit: (root, instance, ast, target) => {
-        module.exports[ast.__type](root, instance, ast, target);
-    },
-
-    build: (root, instance) => {
-        //
-    },
+        build: (root, instance) => {
+            //
+        },
+    };
 };
