85bdb2ecfaa48853507412f863151bfcddf69c3a
hcz <h.cz@qq.com>
2016-09-23 16:32:19 -0400

    gen.js.js -> pass.2.js.js

--- pass.2.js.js
+++ pass.2.js.js
@@ -1,18 +1,18 @@
 'use strict';
 
 module.exports = () => {
-    const generator = {
+    const pass = {
         code: [],
         buffer: [],
 
         write: (line) => {
-            generator.buffer.back().push(line + ';\n');
+            pass.buffer.back().push(line + ';\n');
         },
 
         literal: (root, instance, ast, target) => {
             switch (ast.type) {
                 case 'void': {
-                    generator.write(target('undefined'));
+                    pass.write(target('undefined'));
 
                     break;
                 }
@@ -25,12 +25,12 @@ module.exports = () => {
                 case 'u32':
                 case 'f32':
                 case 'f64': {
-                    generator.write(target(ast.value.toString()));
+                    pass.write(target(ast.value.toString()));
 
                     break;
                 }
                 case 'string': {
-                    generator.write(target(JSON.stringify(ast.value)));
+                    pass.write(target(JSON.stringify(ast.value)));
 
                     break;
                 }
@@ -45,57 +45,57 @@ module.exports = () => {
         },
 
         self: (root, instance, ast, target) => {
-            generator.write(target('self'));
+            pass.write(target('self'));
         },
 
         root: (root, instance, ast, target) => {
-            generator.write(target('root'));
+            pass.write(target('root'));
         },
 
         pathOut: (root, instance, ast, target) => {
-            generator.visitOut(
+            pass.visitOut(
                 root, instance, ast.upper,
                 (value) => {
                     return 'upper = ' + value;
                 }
             );
 
-            generator.write(target('upper.get(' + JSON.stringify(ast.name) + ')'));
+            pass.write(target('upper.get(' + JSON.stringify(ast.name) + ')'));
         },
 
         pathIn: (root, instance, ast, value) => {
-            generator.visitOut(
+            pass.visitOut(
                 root, instance, ast.upper,
                 (value) => {
                     return 'upper = ' + value;
                 }
             );
 
-            generator.write('upper.set(' + JSON.stringify(ast.name) + ', ' + value + ')');
+            pass.write('upper.set(' + JSON.stringify(ast.name) + ', ' + value + ')');
         },
 
         call: (root, instance, ast, before, after, builder) => {
-            generator.visitOut(
+            pass.visitOut(
                 root, instance, ast.callee,
                 (value) => {
                     return 'callee = ' + value;
                 }
             );
 
-            const calleeId = generator.build(ast.instance, builder);
+            const calleeId = pass.build(ast.instance, builder);
 
-            generator.write('inner = new Map()');
-            generator.write('inner.set(\'__func\', ' + calleeId + ')');
+            pass.write('inner = new Map()');
+            pass.write('inner.set(\'__func\', ' + calleeId + ')');
 
-            const returnId = calleeId + '_' + generator.buffer.length;
+            const returnId = calleeId + '_' + pass.buffer.length;
 
-            generator.write('inner.set(\'__outer\', callee)');
-            generator.write('callee = inner');
+            pass.write('inner.set(\'__outer\', callee)');
+            pass.write('callee = inner');
 
             before();
 
             for (const i in ast.outArgs) {
-                generator.visitOut(
+                pass.visitOut(
                     root, instance, ast.callee,
                     (value) => {
                         return 'callee.set(' + JSON.stringify(i) + ', ' + value + ')';
@@ -103,23 +103,23 @@ module.exports = () => {
                 );
             }
 
-            generator.write('callee.set(\'__caller\', self)');
-            generator.write('self = callee');
+            pass.write('callee.set(\'__caller\', self)');
+            pass.write('self = callee');
 
             // call
-            generator.write('func = callee.get(\'__func\')');
-            generator.write('callee.set(\'__func\', ' + returnId + ')');
-            generator.write('func()');
+            pass.write('func = callee.get(\'__func\')');
+            pass.write('callee.set(\'__func\', ' + returnId + ')');
+            pass.write('func()');
 
-            generator.write('}');
-            generator.write('');
-            generator.write('const ' + returnId + ' = () => {');
+            pass.write('}');
+            pass.write('');
+            pass.write('const ' + returnId + ' = () => {');
 
-            generator.write('callee = self');
-            generator.write('self = callee.get(\'__caller\')');
+            pass.write('callee = self');
+            pass.write('self = callee.get(\'__caller\')');
 
             for (const i in ast.inArgs) {
-                generator.visitIn(
+                pass.visitIn(
                     root, instance, ast.callee,
                     'callee.get(' + JSON.stringify(i) + ')'
                 );
@@ -127,21 +127,21 @@ module.exports = () => {
 
             after();
 
-            generator.write('inner = callee');
-            generator.write('callee = inner.get(\'__outer\')');
+            pass.write('inner = callee');
+            pass.write('callee = inner.get(\'__outer\')');
         },
 
         callOut: (root, instance, ast, target) => {
-            generator.call(
+            pass.call(
                 root, instance, ast,
                 () => {
                     // nothing
                 },
                 () => {
-                    generator.write(target('callee.get(\'__result\')'));
+                    pass.write(target('callee.get(\'__result\')'));
                 },
                 (child, ast) => {
-                    generator.visitOut(
+                    pass.visitOut(
                         root, child, ast,
                         (value) => {
                             return 'self.set(\'__result\', ' + value + ')';
@@ -152,16 +152,16 @@ module.exports = () => {
         },
 
         callIn: (root, instance, ast, value) => {
-            generator.call(
+            pass.call(
                 root, instance, ast,
                 () => {
-                    generator.write('callee.set(\'__input\', ' + value + ')');
+                    pass.write('callee.set(\'__input\', ' + value + ')');
                 },
                 () => {
                     // nothing
                 },
                 (child, ast) => {
-                    generator.visitIn(
+                    pass.visitIn(
                         root, child, ast,
                         'self.get(\'__input\')'
                     );
@@ -171,7 +171,7 @@ module.exports = () => {
 
         visitOut: (root, instance, ast, target) => {
             // TODO: check ast.__type
-            generator[ast.__type](
+            pass[ast.__type](
                 root, instance, ast,
                 target
             );
@@ -179,7 +179,7 @@ module.exports = () => {
 
         visitIn: (root, instance, ast, value) => {
             // TODO: check ast.__type
-            generator[ast.__type](
+            pass[ast.__type](
                 root, instance, ast,
                 value
             );
@@ -188,22 +188,22 @@ module.exports = () => {
         build: (instance, builder) => {
             // TODO: remove duplicated
 
-            const id = 'func_' + generator.code.length;
+            const id = 'func_' + pass.code.length;
 
-            generator.buffer.push([]);
+            pass.buffer.push([]);
 
-            generator.write('const ' + id + ' = () => {');
+            pass.write('const ' + id + ' = () => {');
 
             builder(instance, instance.impl2);
 
-            generator.write('}');
-            generator.write('');
+            pass.write('}');
+            pass.write('');
 
-            generator.code.push(generator.buffer.pop());
+            pass.code.push(pass.buffer.pop());
 
             return id;
         },
     };
 
-    return generator;
+    return pass;
 };
