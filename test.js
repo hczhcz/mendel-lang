174c7133221291676ae25fb473b7fc4c5f134981
hcz <h.cz@qq.com>
2016-11-19 13:06:34 -0500

    apply

--- test.js
+++ test.js
@@ -7,6 +7,7 @@ const ast1 = require('./ast.1');
 const ast2 = require('./ast.2');
 const boot1 = require('./boot.1');
 const boot2js = require('./boot.2.js');
+const boot2c = require('./boot.2.c');
 
 const a1 = ast1.call(ast1.lookup('__do'), [
     // var a = 'hello';
@@ -37,6 +38,7 @@ const a1 = ast1.call(ast1.lookup('__do'), [
 
 const b1 = boot1();
 const b2js = boot2js();
+const b2c = boot2c();
 
 b1.namedModule(
     '__do', 'const', ast1.code(
@@ -46,6 +48,9 @@ b1.namedModule(
                 return ast2.nativeOut(
                     {
                         js: (pass, target) => {
+                            // nothing
+                        },
+                        c: (pass, target) => {
                             // nothing
                         },
                     },
@@ -74,6 +79,9 @@ b1.namedModule(
                         js: (pass, target) => {
                             pass.write('__self.set(\'a\', __self.get(\'b\'))');
                         },
+                        c: (pass, target) => {
+                            pass.write('__self.set(\'a\', __self.get(\'b\'))');
+                        },
                     },
                     typeinfo.basic('null')
                 );
@@ -93,6 +101,9 @@ b1.namedModule(
                 return ast2.nativeOut(
                     {
                         js: (pass, target) => {
+                            pass.write('console.log(__self.get(\'a\'))');
+                        },
+                        c: (pass, target) => {
                             pass.write('console.log(__self.get(\'a\'))');
                         },
                     },
@@ -126,6 +137,35 @@ try {
     fs.writeFile(
         'test_gen.js',
         headjs + b2js.render() + m2js,
+        (err) => {
+            //
+        }
+    );
+
+    const headc = '#include <iostream>\n'
+        + '\n'
+        + 'struct struct_head {\n'
+        + '    void (*__func)();\n'
+        + '    struct struct_head *__caller;\n'
+        + '    struct struct_head *__outer;\n'
+        + '};\n'
+        + '\n'
+        // TODO: structs here
+        + '\n'
+        + 'struct struct_head *__upper;\n'
+        + 'struct struct_head *__inner;\n'
+        + 'struct struct_head *__callee;\n'
+        + 'struct struct_0 __root_struct;\n'
+        + 'struct struct_head *__root ='
+            + '(struct struct_head *) &__root_struct;\n'
+        + 'struct struct_head *__self = __root;\n'
+        + '\n';
+        // TODO: init members of __root_struct
+
+    const m2c = b2c.module(a2);
+    fs.writeFile(
+        'test_gen.c',
+        headc + b2c.render() + m2c,
         (err) => {
             //
         }
