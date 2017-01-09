be44cfa4143fd7b9a25a6ab2dca9ab4979164165
hcz <h.cz@qq.com>
2017-01-10 00:18:59 +0800

    rename mendel.driver.js -> mendel.js

--- mendel.js
+++ mendel.js
@@ -6,8 +6,8 @@ const fs = require('fs');
 const type = require('./1/type');
 const boot0 = require('./0/boot');
 const boot1 = require('./1/boot');
-const boot2js = require('./2.js/boot');
-const boot2c = require('./2.c/boot');
+const boot3js = require('./3.js/boot');
+const boot3c = require('./3.c/boot');
 const libcore = require('./lib.core');
 
 if (process.argv.length < 4) {
@@ -18,14 +18,14 @@ const outFile = fs.openSync(process.argv[3], 'w');
 
 const root = type.instance('out');
 
-const b2js = boot2js(
+const b3js = boot3js(
     root,
     (data) => {
         fs.writeSync(outFile, data);
     }
 );
 
-// const b2js = boot2js(
+// const b3js = boot3js(
 //     root,
 //     (() => {
 //         const doEvalGen = 'doEval = (data) => {\n'
@@ -33,7 +33,7 @@ const b2js = boot2js(
 //             + '};\n'
 //             + '\n';
 
-//         let doEval;
+//         let doEval = null;
 
 //         eval(doEvalGen);
 
@@ -43,11 +43,11 @@ const b2js = boot2js(
 //     })()
 // );
 
-// const b2c = boot2c();
+// const b3c = boot3c();
 
 const b1 = boot1(
     root,
-    b2js.newInstance, b2js.execute, b2js.export
+    b3js.newInstance, b3js.execute, b3js.export
 );
 
 const b0 = boot0(
@@ -57,4 +57,4 @@ const b0 = boot0(
 libcore(b1);
 
 b0.execute(String(fs.readFileSync(process.argv[2])));
-b2js.collect();
+b3js.collect();
